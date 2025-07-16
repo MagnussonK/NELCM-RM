42ae270dcf8570f7800e8d264171630edce3fa66
Kris Magnusson <kmagnusson@comcast.net>
2025-07-16 18:06:46 -0500

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -4,4 +4,4 @@ from app import app # Imports your Flask app instance
 
 def handler(event, context):
     """This is the handler that the Serverless Framework will deploy."""
-    return serverless_wsgi.handle(app, event, context)
+    return serverless_wsgi(app, event, context)
